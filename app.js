7cb108e03e93c02ad6ce1941d6657f72adab1881
dacol <da.colcha@hotmail.com>
2024-01-17 21:47:55 -0500

    Cambios sección 2

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-alert('Hola Mundo!');
-let numeroUsuario = prompt('Me indicas un número, por favor.');
+let numeroSecreto = 8;
+let numeroUsuario = prompt('Me indicas un número entre 1 y 10, por favor.');
+
+/* 
+    Este código compara los números 
+*/
+if (numeroUsuario == numeroSecreto) {
+    // Acertamos el número
+    alert(`Acertaste, el número es: ${numeroUsuario}` );
+} else{
+    // La condición es falsa
+    alert('Lo siento, no acertaste el número.');
+}
+
+console.log(numeroUsuario);
